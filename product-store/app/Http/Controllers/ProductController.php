72cd05bfac00e8707e170663ccf7f0d57adec4e9
mikel <mikelgarciafuente@gmail.com>
2022-10-14 09:50:32 +0200

    14 oct

--- product-store/app/Http/Controllers/ProductController.php
+++ product-store/app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = \App\Models\Product::all();
+        //$products = \App\Models\Product::all();
+        $products = Product::all(); // 'use App\Models\Product' needed
         return view('viewproducts', ['allProducts' => $products]);
 
     }
--- product-store/routes/web.php
+++ product-store/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
