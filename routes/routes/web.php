3dd32c0b08fcb85c5ba9cd3969110bf8b5febe65
mgarciafuente <mikelgarciafuente@gmail.com>
2022-10-24 21:49:47 +0200

    routes exercise started, not working

---
+++ routes/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+Route::view('/contacto', 'home.contacto');
+Route::view('/recent-posts/{days ago}', 'posts.recientes.index');
+Route::view('/posts/{id}', 'posts.show-posts');
